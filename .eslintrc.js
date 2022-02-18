5f3adb5c7199c8308ad4eb93fd2d8fcdd14cdc9a
James Joseph <james.joseph@slalom.com>
2022-02-18 15:06:41 -0700

    Added storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,30 +5,25 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    jest: true,
+    jest: true
   },
   parserOptions: {
     ecmaVersion: 2020,
     ecmaFeatures: {
       impliedStrict: true,
-      jsx: true,
+      jsx: true
     },
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  rules: {
-    // You can do your customizations here...
+  rules: {// You can do your customizations here...
     // For example, if you don't want to use the prop-types package,
     // you can turn off that recommended rule with: 'react/prop-types': ['off']
-  },
+  }
 };
